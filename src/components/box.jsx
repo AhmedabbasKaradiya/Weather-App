d1c442157eace65d0866d1a33ac9a837a6d8e320
AhmedabbasKaradiya <kahmedabbas44@gmail.com>
2024-10-05 21:43:37 +0530

    initial commit

---
+++ src/components/box.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './box.css'
+const box = () => {
+     return (
+          <div className='boxes'>
+               <div className="box">
+                    <p>Wind</p>
+                    <h2>6 km/h</h2>
+                    <p>East</p>
+               </div>
+          </div>
+     )
+}
+
+export default box
